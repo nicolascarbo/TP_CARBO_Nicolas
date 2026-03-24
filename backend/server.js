2dff710cef2702ae2df1585d5d01cd570e8ea7a0
nicolascarbo <nico.carbo@orange.fr>
2026-03-24 13:15:21 +0100

    (TP2_Partie_4(Bonus_C)): add middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import usersRouter from './routes/users.js'
+import loggerMiddleware from './middleware/logger.js'
 
 const app = express()
 const port = 3001
 
 app.use(express.json())
+
+app.use(loggerMiddleware)
 app.use('/api/users', usersRouter)
 
 app.listen(port, () => {
