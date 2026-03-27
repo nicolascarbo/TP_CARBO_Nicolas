985ab27bae950e87c3df17664be80f834d7abb41
nicolascarbo <nico.carbo@orange.fr>
2026-03-27 11:36:00 +0100

    (TP4_Partie_3(3.1)): init navbar.jsx:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,49 @@
-import './App.css'
+import { useCallback, useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import userService from "./services/userService";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  const fetchUsers = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await userService.getAll();
+
+      setUsers(response.data.data);
+      setTotalCount(response.data.totalCount);
+    } catch (error) {
+      console.error("Erreur lors de la récupération :", error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar count={totalCount} />
+
+      <main style={{ padding: "0 2rem" }}>
+        {loading ? (
+          <p>Chargement...</p>
+        ) : (
+          <ul>
+            {users.map((user) => (
+              <li key={user._id}>
+                {user.name} ({user.email})
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
     </div>
-  )
-}
-export default App
+  );
+};
+
+export default App;
