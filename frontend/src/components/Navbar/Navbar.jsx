8a778106a9dd8a77bbdc7b78b0b51381d0bc3fdf
nicolascarbo <nico.carbo@orange.fr>
2026-03-27 11:36:00 +0100

    (TP4_Partie_3(3.1)): init navbar.jsx:

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'; 
+
+const Navbar = ({ count }) => {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">Gestion des Utilisateurs</h1>
+      <div className="badge-count">
+        {count} utilisateurs
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
