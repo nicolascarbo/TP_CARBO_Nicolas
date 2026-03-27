f525141f87568ff20123f299b2a58f34de6a65be
nicolascarbo <nico.carbo@orange.fr>
2026-03-27 11:13:16 +0100

    (TP4_Partie_2): init axios, and method http

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/api/users', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const userService = {
+  getAll: (params) => {
+    return api.get('/', { params });
+  },
+
+  getById: (id) => {
+    return api.get(`/${id}`);
+  },
+
+  create: (data) => {
+    return api.post('/', data);
+  },
+
+  update: (id, data) => {
+    return api.put(`/${id}`, data);
+  },
+
+  remove: (id) => {
+    return api.delete(`/${id}`);
+  }
+};
+
+export default userService;
